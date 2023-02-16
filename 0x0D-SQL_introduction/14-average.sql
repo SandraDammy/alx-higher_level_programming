01305cd12a35e96eb586f9d198633cc983559035
SandraDammy <oluwadamilolasandraodujirin@gmail.com>
2023-02-16 15:02:37 -0800

    task

--- 0x0D-SQL_introduction/14-average.sql
+++
@@ -1,3 +0,0 @@
--- Script that computes the score average of all records in a table
--- Query to computes the score average of all records in the table second_table
-SELECT AVG(score) AS average FROM second_table;
--- 0x0D-SQL_introduction/8-count_89.sql
+++
@@ -1,3 +0,0 @@
--- Displays the number of records
--- Query to display the number of records with id = 89 in a table
-SELECT COUNT(*) as id FROM first_table WHERE id=89;
